15a872ab53e51170655c7df347616d44b91c4502
abuhasanmahmud <mahmud.me420@gmail.com>
2023-11-03 23:57:10 +0600

    Fea

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import ImageGallery from "./componets/ImageGallery";
 import Drag from "./componets/Drag";
 
 export default function Home() {
+  console.log("ss");
   return (
     <>
       <ImageGallery />
